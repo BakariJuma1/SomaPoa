ceee63e81ee71d6f4a6b914116b07617b1cef139
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-17 19:08:17 +0300

    fix:Match my frontend  json response with backend to username

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -53,7 +53,7 @@ const Register = () => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            username:formData.username || formData.email, 
+            username:formData.username, 
             otp: formData.otp,
           }),
         });
