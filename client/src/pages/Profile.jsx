b2a1fc5155ad05a7ff5a86126240dc3dc35d6624
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-17 17:23:59 +0300

    fix:Adding resend to my requirements

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import{ useState } from "react";
 import { useAuth } from "../context/AuthProvider";
 import "../assets/styles/profile.css"; // Assuming you're putting styles here
 
